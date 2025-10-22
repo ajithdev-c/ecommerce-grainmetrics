5f348699d189d22014a8d6aa74ecde96c6272d61
ajithdev-c <ajithdev.c@grainmetrics.com>
2025-10-22 21:04:40 +0530

    migration files

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251022_074003 from './20251022_074003';
 import * as migration_20251022_121557 from './20251022_121557';
+import * as migration_20251022_153227 from './20251022_153227';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20251022_121557.up,
     down: migration_20251022_121557.down,
-    name: '20251022_121557'
+    name: '20251022_121557',
+  },
+  {
+    up: migration_20251022_153227.up,
+    down: migration_20251022_153227.down,
+    name: '20251022_153227'
   },
 ];
